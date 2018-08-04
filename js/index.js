142922a48f6eb04fddd6da0a846a968afa9002ed
Piotr Nastaj <piotr@nastaj.net>
2018-08-04 13:16:01 +0200

    Update html,css,js files

--- js/index.js
+++ js/index.js
@@ -1,53 +1,41 @@
-var playerScore = 0;
-var computerScore = 0;
-var gameLength;
-var gameLengthElement = document.getElementById("game_length");
-var outputElement = document.getElementById("output");
-var playerScoreElement = document.getElementById("player_score");
-var computerScoreElement = document.getElementById("computer_score");
-var gameButtonElements = document.getElementsByClassName("game_button");
-var rockElement = document.getElementById("rock");
-var paperElement = document.getElementById("paper");
-var scissorsElement = document.getElementById("scissors");
-var newGameButtonElement = document.getElementById("new-game-button");
-
-for (x = 0; x < 3; x++) {
-  gameButtonElements[x].disabled = true;
-}
-
-/*
 var DOM = {
- gameLength: document.getElementById("game_length"),
- output: document.getElementById("output"),
- playerScore: document.getElementById("player_score"),
- computerScore: document.getElementById("computer_score"),
- gameButton: document.getElementsByClassName("game_button"),
- rock: document.getElementById("rock"),
- paper: document.getElementById("paper"),
- scissors: document.getElementById("scissors"),
- newGameButton: document.getElementById("new-game-button"),
+  gameButtons: document.getElementsByClassName("game_button"),
+  LengthOfGameInfo: document.getElementById("game_length_info"),
+  output: document.getElementById("output"),
+  playerScore: document.getElementById("player_score"),
+  computerScore: document.getElementById("computer_score"),
+  newGameButton: document.getElementById("new-game-button"),
 }
 
-var gameStats = {
+var params = {
   playerScore: 0,
   computerScore: 0,
   gameLength: 0,
+  roundsPlayed: 0,
+  buttonsActive: false,
+  gameEnded: false,
+
 }
-*/
+
+for (x = 0; x < 3; x++) {
+  DOM.gameButtons[x].disabled = true;
+}
+
+DOM.newGameButton.addEventListener("click", newGame);
 
 function newGame() {
   resetGame();
   (function askForGameLength() {
-    gameLength = prompt("How many won rounds should a game last?");
-    console.log(gameLength);
-    if (!isNaN(parseInt(gameLength, 10)) && parseInt(gameLength, 10) > 0) {
-      gameLengthElement.innerHTML =
+    params.gameLength = prompt("How many won rounds should a game last?");
+    console.log(params.gameLength);
+    if (!isNaN(parseInt(params.gameLength, 10)) && parseInt(params.gameLength, 10) > 0) {
+      DOM.LengthOfGameInfo.innerHTML =
         "The game lasts until " +
-        gameLength +
+        params.gameLength +
         " round" +
-        (gameLength == 1 ? " is" : "s are") +
+        (params.gameLength == 1 ? " is" : "s are") +
         " won.<br>";
-    } else if (gameLength == null) {} 
+    } else if (params.gameLength == null) {} 
     else {
       return askForGameLength();
     }
@@ -59,60 +47,35 @@ function randomChoice() {
 }
 
 function displayText(text) {
-  outputElement.innerHTML += text + "<br>";
+  DOM.output.innerHTML += text + "<br>";
 }
 
 function resetGame() {
   for (x = 0; x < 3; x++) {
-    gameButtonElements[x].disabled = false;
+    DOM.gameButtons[x].disabled = false;
   }
-  playerScore = computerScore = 0;
-  playerScoreElement.innerHTML = playerScore;
-  computerScoreElement.innerHTML = computerScore;
-  outputElement.innerHTML = "";
+  params.playerScore = params.computerScore = 0;
+  DOM.playerScore.innerHTML = params.playerScore;
+  DOM.computerScore.innerHTML = params.computerScore;
+  DOM.output.innerHTML = "";
 }
 
-/* OLD VERSION OF POSTRESULT
- 
 function postResult() {
-  playerScoreElement.innerHTML = playerScore;
-  computerScoreElement.innerHTML = computerScore;
-  if (playerScore == gameLength) {
-    for (x=0 ; x <3 ; x++){
-      gameButtonElements[x].disabled = true;
-    };
-    displayText('<strong>CONGRATULATIONS, YOU WON ' + gameLength + ' ROUND' + (gameLength == 1 ? '' : 'S') + '. GAME OVER.');
-    setTimeout(function(){
-      displayText('Please press the New Game button');
-    },2000);
-  }
-  else if (computerScore == gameLength) {
-    for (x=0 ; x <3 ; x++){
-      gameButtonElements[x].disabled = true;
-    };
-    displayText('<strong>GAME OVER. YOUR OPONENT WON ' + gameLength + ' ROUND' + (gameLength == 1 ? '' : 'S') + '.');
-    setTimeout(function(){
-      displayText('Please press the New Game button');
-    },2000);
-  }
-  */
-
-function postResult() {
-  playerScoreElement.innerHTML = playerScore;
-  computerScoreElement.innerHTML = computerScore;
-  if (playerScore == gameLength || computerScore == gameLength) {
+  DOM.playerScore.innerHTML = params.playerScore;
+  DOM.computerScore.innerHTML = params.computerScore;
+  if (params.playerScore == params.gameLength || params.computerScore == params.gameLength) {
     for (x = 0; x < 3; x++) {
-      gameButtonElements[x].disabled = true;
+      DOM.gameButtons[x].disabled = true;
     }
     displayText(
       "<strong>" +
-        (playerScore == gameLength ? "CONGRATULATIONS, " : "") +
+        (params.playerScore == params.gameLength ? "CONGRATULATIONS, " : "") +
         "YOU" +
-        (playerScore == gameLength ? "" : "R OPPONENT") +
+        (params.playerScore == params.gameLength ? "" : "R OPPONENT") +
         " WON " +
-        gameLength +
+        params.gameLength +
         " ROUND" +
-        (gameLength == 1 ? "" : "S") +
+        (params.gameLength == 1 ? "" : "S") +
         ". GAME OVER.</strong>"
     );
     setTimeout(function() {
@@ -121,63 +84,7 @@ function postResult() {
   }
 }
 
-/* PRZEROBIC NA FUNKCJE PLAYER, FUNKCJE COMPUTER I FUNKCJE POROWNANIE -> WYNIK */
-/*
-OLD VERSION OF BUTTON CLICKS
-
-rockElement.addEventListener('click', function(){
-  switch(randomChoice()){
-    case 0:
-      displayText('TIE: you played ROCK, computer played ROCK');
-      break;
-    case 1:
-      displayText('COMPUTER WON: you played ROCK, computer played PAPER');
-      computerScore++;
-      break;
-    case 2:
-      displayText('YOU WON: you played ROCK, computer played SCISSORS');
-      playerScore++;
-      break;
-    }
-  postResult();
-  });
-
-paperElement.addEventListener('click', function(){
-  switch(randomChoice()){
-    case 0:
-      displayText('YOU WON: you played PAPER, computer played ROCK');
-      playerScore++;
-      break;
-    case 1:
-      displayText('TIE: you played PAPER, computer played PAPER');
-      break;
-    case 2:
-      displayText('COMPUTER WON: you played PAPER, computer played SCISSORS');
-      computerScore++;
-      break;
-    }
-  postResult();
-  });
-
-scissorsElement.addEventListener('click', function(){
-  switch(randomChoice()){
-    case 0:
-      displayText('COMPUTER WON: you played SCISSORS, computer played ROCK');
-      computerScore++;
-      break;
-    case 1:
-      displayText('YOU WON: you played SCISSORS, computer played PAPER');
-      playerScore++;
-      break;
-    case 2:
-      displayText('TIE: you played SCISSORS, computer played SCISSORS');
-      break;
-    };
-  postResult();
-});
-*/
-
-Array.from(gameButtonElements).forEach(function(element) {
+Array.from(DOM.gameButtons).forEach(function(element) {
   element.addEventListener("click", function() {
     let playerChoice = this.id == "rock" ? 0 : this.id == "paper" ? 1 : 2;
     let computerChoice = randomChoice();
@@ -187,14 +94,14 @@ Array.from(gameButtonElements).forEach(function(element) {
         ? "Tie. You both played " + clickedButton + "."
         : computerChoice - playerChoice == 1 ||
           computerChoice - playerChoice == -2
-          ? (computerScore++,
+          ? (params.computerScore++,
             "Computer won. You played " +
               clickedButton +
               ", the computer played " +
               (playerChoice == 0
                 ? "paper."
                 : playerChoice == 1 ? "scissors." : "rock."))
-          : (playerScore++,
+          : (params.playerScore++,
             "You won. You played " +
               clickedButton +
               ", the computer played " +
@@ -206,4 +113,7 @@ Array.from(gameButtonElements).forEach(function(element) {
   });
 });
 
-newGameButtonElement.addEventListener("click", newGame);
+var showModal = function(event){
+  event.preventDefault();
+  document.querySelector('#modal-overlay').classList.add('show');
+};
